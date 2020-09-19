33c7c5133710306e8fde7261017a50237b38d24e
UnderdoneLizard <mitchelljmarrs@gmail.com>
2020-09-19 18:37:42 -0400

    made game schema

--- models/Game.js
+++ models/Game.js
@@ -1,20 +1,23 @@
 //bring in mongoose 
-
+const mongoose = require('mongoose');
 //make a schema 
-
-/* {
+const gameSchema = new mongoose.Schema(
+{
     title: {type: String, required: true, unique: true},
     releaseDate: {type: Date, required: true},
     description: {type: String, required: true},
     img: {type: String, required: true},
-    genre: [{type: String, required: true}]
-    link: {type: String, required: true}
+    genre: [{type: String, required: true}],
+    link: {type: String, required: true},
     dev: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: "Dev",
     }]
-}, do time stamps */
+},{
+    timestamps: true
+})
 
 //make model 
-
+const Game = mongoose.model("Game", gameSchema);
 //export the model 
+module.exports = Game;
