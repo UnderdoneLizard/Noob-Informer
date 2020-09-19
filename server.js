c7028b7b7b5a12cf3ce8db0ab476b9bf4fa45722
Frankie Barreto <barretoacquire@gmail.com>
2020-09-19 14:41:53 -0700

    added external modules, internal modules, and  middleware to server

--- server.js
+++ server.js
@@ -1,34 +1,51 @@
-//External Modules
+// SECTION External Modules
 const express = require('express');
-
 //bring method override express session and connect mongo
+const methodOverride = require("method-over");
+const session = require("express-session");
 //require path
+const path = require('path');
 
-//Internal modules 
+const MongoStore = require("connect-mongo")('session');
+
+
+
+// SECTION Internal modules 
 //require db and controllers index 
+const db = require('./models')
+const controller = require('./controllers')
 
 //Instanced modules
 const app = express();
 
-//Config
+// Config
 const PORT = 3000;
-//pass user info to all files with locals
-//set engine to ejs
 
-//Middleware
-//app.use(express.static(path.join(__dirname, 'public')));
-//use url encoded 
-//method override
-//set up session (look at express blog)
+
+// TODO pass user info to all files with locals 
+
+//set engine to ejs
+app.set('view engine', 'ejs');
+
+
+// SECTION Middleware
+app.use(express.static(path.join(__dirname, 'public')));
+// use url encoded
+app.use(express.urlencoded({extended: true}));
+// method override
+app.use(methodOverride("_method"));
+// TODO set up session (look at express blog)
+
+
 
 //Routes
 //base route that will take user to the home page 
-
 //auth routes
-
+// app.use('/', controller.auth)
 //game routes
-
+app.use('/', controller.game);
 //dev routes
+app.use('/', controller.dev);
 
 //Server Listener
 app.listen(PORT, () => {
