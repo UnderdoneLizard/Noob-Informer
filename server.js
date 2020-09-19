dcee4b964df0d1ee35e5e2d2c449a85f1e09d5a2
UnderdoneLizard <mitchelljmarrs@gmail.com>
2020-09-19 13:14:09 -0400

    tsudocode

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
 //External Modules
 const express = require('express');
+//bring method override express session and connect mongo
+//require path
+
 //Internal modules 
+//require db and controllers index 
 
 //Instanced modules
 const app = express();
+
 //Config
 const PORT = 3000;
+//pass user info to all files with locals
+//set engine to ejs
+
 //Middleware
+//app.use(express.static(path.join(__dirname, 'public')));
+//use url encoded 
+//method override
+//set up session (look at express blog)
 
 //Routes
+//base route that will take user to the home page 
+
+//auth routes
+
+//game routes
+
+//dev routes
 
 //Server Listener
 app.listen(PORT, () => {
