92a8f362bc2f5d3cc5f8f88173d62427bff4a7a5
Frankie Barreto <barretoacquire@gmail.com>
2020-09-19 15:45:08 -0700

    created dev schema

--- models/Dev.js
+++ models/Dev.js
@@ -1,7 +1,25 @@
 //connect mongoose 
+const mongoose = require("mongoose");
 
 //make schema 
+const devSchema = new mongoose.Schema(
+    {
+        title: {type: String, required: true},
+        games: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Game',
+        }],
+        description: {type: String, required: true},
+        icon: {type: String, required: true},
+        link: {type: String, required: true}
+    },
+    {
+        timestamps: true,
+    }
+)
+const Dev = mongoose.model("Dev", devSchema);
 
+module.exports = Dev;
 /* 
 {
     name: {type: String, required: true},
