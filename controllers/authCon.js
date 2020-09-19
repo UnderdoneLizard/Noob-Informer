fca02202e00feb72e8bc17b2bba3843d421f2e74
Frankie Barreto <barretoacquire@gmail.com>
2020-09-19 11:41:56 -0700

    pseudo coded authController

---
+++ controllers/authCon.js
@@ -0,0 +1,41 @@
+// const express = require("express");
+// const router = express.Router();
+// const db = require("../models");
+// const bcrypt = require("bcryptjs"); // ?????????/ hashing function
+ 
+//make them all async try catch
+
+// base path /
+
+//register form
+    // auth/register render form
+
+
+// register post 
+    // search db to see if user already exits (using email/username)
+    // if user exists send back error (already exists)
+    // if no user is found send hash+salt password
+    // redirect to login
+
+
+// login form (/login)
+    // render login form 
+
+
+// login post 
+    // db findOne username/email
+        // if email does not matches 
+        // return error email || password incorrect
+        
+        // if email exists check password
+        // if password correct add user to session
+        // redirect homepage
+        
+        // if password incorrect 
+        // return error email || password incorrect
+
+// logout aka destroy session
+    // use req.sessions.destroy();
+    // ten redirect to home page
+
+// modules.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,2 @@
 //export all the required controllers 
+
