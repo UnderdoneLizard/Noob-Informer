6431c1313411e32b477f58e4db1672dee3574530
UnderdoneLizard <mitchelljmarrs@gmail.com>
2020-09-21 21:15:08 -0400

    user model complete

--- models/User.js
+++ models/User.js
@@ -1,7 +1,24 @@
 //include mongoose
-
+const mongoose = require('mongoose');
 //make schema 
+const userSchema = mongoose.Schema({
+    username: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    favGames: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Game"
+    }],
+    favDevs: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Dev"
+    }]
+},{
+    timestamps: true
+})
 
+const User = mongoose.model('User', userSchema);
+module.exports = User;
 /* 
 {
     username: {type: String, required: true},
